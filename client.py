30030a5c9042e8d5e424946f1ec345c0ef53b6ea
0x00991 <129187044+0x00991@users.noreply.github.com>
2023-08-03 23:26:12 +0900

    Initial Commit

---
+++ client.py
@@ -0,0 +1,94 @@
+import pyautogui
+from websockets.sync.client import connect
+from io import BytesIO
+from PIL import Image
+from threading import Thread
+import time
+
+SERVER_IP = "127.0.0.1"
+PORT = "40002"
+
+LAST_SENT = -1
+
+_ALERT_THREADS = []
+
+SCR_INTERVAL = 2
+
+RESIZE_IMAGE = True
+RESIZE_TO_720p = True
+
+
+captsize = []
+if RESIZE_TO_720p:
+    captsize = [1280, 720]
+
+def time_calc(start, end):
+    return round((end-start)*1000, 1)
+
+# todo: tkinter로 변경
+def show_alert(message):
+    thread = Thread(target=pyautogui.alert, args=(message,))
+    thread.daemon = True
+    thread.start()
+    _ALERT_THREADS.append(thread)
+
+def sync_server():
+    global LAST_SENT
+    
+    while True:
+        try:
+            with connect(f"ws://{SERVER_IP}:{PORT}", max_size=32*2**20) as ws:
+                print("[INFO] Reconnected!")
+                ws.send("PING|Connected")
+                while True:
+                    data = ws.recv()
+                        
+                    if data.startswith("PING|"):
+                        if data == "PING|RES":
+                            ws.send("PONG|")
+                            
+                    # str
+                    if data.startswith("SCREEN|"):
+                        if len(data.replace("SCREEN|", "", 1).split("x")) == 2:
+                            captsize.clear()
+                            captsize.extend(
+                                list(map(int, data.replace("SCREEN|", "", 1).split("x")))
+                            )
+
+
+                    if data.startswith("SCREEN|") or LAST_SENT+SCR_INTERVAL < time.time():
+                        bytesio = BytesIO()
+                        start_time = time.time()
+                        
+                        _img = pyautogui.screenshot()
+                        med_time = time.time()
+                        
+                        if RESIZE_IMAGE:
+                            if _img.size[0] > captsize[0] or _img.size[1] > captsize[1]:
+                                _img = _img.resize(tuple(captsize))
+                        med2_time = time.time()
+                        _img.save(bytesio, "PNG")
+                        med3_time = time.time()
+                        bytesio.seek(0)
+                        ws.send(b"SCREEN|"+bytesio.read())
+                        end_time = time.time()
+                        print(f"[INFO] Screenshot Sent (Scr {time_calc(start_time, med_time)}ms / Rez {time_calc(med_time, med2_time)}ms / Save {time_calc(med2_time, med3_time)}ms / Send {time_calc(med2_time, end_time)}ms / Tot {time_calc(start_time, end_time)}ms)")
+                        LAST_SENT = time.time()
+                        del bytesio, _img
+                        continue
+
+                    if data.startswith("PONG|"):
+                        continue
+
+                    if data.startswith("MESSAGE|"):
+                        data = data.split("|")
+                        content = data[1]
+                        show_alert(content)
+                        continue
+        except KeyboardInterrupt:
+            break
+        except Exception as e:
+            print("Error", e)
+
+
+sync_server()
---
+++ server.py
@@ -0,0 +1,80 @@
+from websockets.server import serve
+import asyncio
+from PIL import Image, ImageTk
+from tkinter import Tk, Label
+from io import BytesIO
+from threading import Thread
+import time
+Clients = []
+
+images = []
+
+SCREENSHOT_INTERVAL = 2
+UPDATE_INTERVAL = 0.2
+captsize = "1280x720"
+
+async def process(ws):
+    Clients.append(ws)
+    while True:
+        try:
+            data = await asyncio.wait_for(ws.recv(), SCREENSHOT_INTERVAL)
+        except TimeoutError:
+            data = "SENDSCREEN|"
+        except Exception as e:
+            if not isinstance(e, TimeoutError):
+                print("error:", e)
+                if ws in Clients:
+                    Clients.remove(ws)
+                return
+
+        if type(data) != str:
+            if data.startswith(b"SCREEN|"):
+                img_bytes = data[7:]
+                if len(img_bytes) > 20*2**20:
+                    await ws.send("ERROR|ImageTooBig")
+                    continue
+                images.append(BytesIO(img_bytes))
+                if len(images) > 1:
+                    images.pop(0)
+                continue
+            await ws.send("ERROR|Unexpected")
+            continue
+        
+        if data == "SENDSCREEN|":
+            await ws.send(f"SCREEN|{captsize}")
+            continue
+
+        # str
+        if data.startswith("PING|"):
+            await ws.send("PONG|OK")
+            continue
+        
+async def main():
+    async with serve(process, "0.0.0.0", 40002, max_size=32*2**20):
+        print("Server started. Listening: 0.0.0.0:40002")
+        await asyncio.Future()
+
+thread = Thread(target=asyncio.run, args=(main(),), daemon=True)
+thread.start()
+
+root = Tk()
+root.geometry("1280x720")
+
+img = ImageTk.PhotoImage(Image.new("RGB", (1280, 720), (0, 0, 0)))
+panel = Label(root, image=img)
+panel.pack(side="bottom", fill="both", expand="no")
+
+def openNresize(img: BytesIO, size: tuple):
+    I = Image.open(img)
+    if I.size[0] == size[0] and I.size[1] == size[1]:
+        return I
+    return I.resize(size)
+
+while True:
+    if images:
+        captsize = root.geometry().split("+")[0]
+        scrsize = list(map(int, captsize.split("x")))
+        img = ImageTk.PhotoImage(openNresize(images[0], scrsize))
+        panel.configure(image=img)
+    time.sleep(UPDATE_INTERVAL)
+    root.update()
